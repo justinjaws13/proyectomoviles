eb43578cb3814fa84b09b50ee9ed0e99d9ea889b
Justin Quijada Hernandez <justin_quijada@hotmail.com>
2024-09-24 19:50:40 -0400

    proyecto movil, solo pude poner un boton porque no me dejaba correrlo

---
+++ app/src/main/java/edu/pucmm/icc451/MainActivity.java
@@ -0,0 +1,46 @@
+package edu.pucmm.icc451;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        FirebaseAuth.AuthStateListener authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+                if (currentUser != null){
+                    Log.i( "AuthStateListener",  "User: " + currentUser.getEmail());
+                }
+            }
+        };
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth.addAuthStateListener(authStateListener);
+
+        FirebaseUser currentUser = auth.getCurrentUser();
+
+
+    }
+}
