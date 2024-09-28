3d231987001358036c4685065f0897fbe286de9d
Justin Quijada Hernandez <justin_quijada@hotmail.com>
2024-09-28 10:33:07 -0400

    login

--- proyectoMovil1/app/src/main/java/edu/pucmm/icc451/AuthActivity.java
+++ proyectoMovil1/app/src/main/java/edu/pucmm/icc451/AuthActivity.java
@@ -1,17 +1,37 @@
 package edu.pucmm.icc451;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AuthActivity extends AppCompatActivity {
 
+    private FirebaseAuth auth;
+    private Button registrarButton, cerrarSesionButton, loginButton;
+    private EditText emailEditText, passwordEditText;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FirebaseApp.initializeApp(this);
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_auth);
@@ -20,5 +40,80 @@ public class AuthActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        auth = FirebaseAuth.getInstance();
+        registrarButton = findViewById(R.id.registrarButton);
+        cerrarSesionButton = findViewById(R.id.cerrarSesionButton);
+        loginButton = findViewById(R.id.loginButton);
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+
+        setup();
+    }
+
+    private void setup() {
+        setTitle("Autenticación");
+
+        registrarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!emailEditText.getText().toString().isEmpty() && !passwordEditText.getText().toString().isEmpty()) {
+                    auth.createUserWithEmailAndPassword(
+                            emailEditText.getText().toString(),
+                            passwordEditText.getText().toString()
+                    ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                showHome(task.getResult().getUser().getEmail() != null ? task.getResult().getUser().getEmail() : "", ProviderType.BASIC, AuthActivity.this);
+                            } else {
+                                showAlert();
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!emailEditText.getText().toString().isEmpty() && !passwordEditText.getText().toString().isEmpty()) {
+                    signInWithEmailAndPass(emailEditText.getText().toString(), passwordEditText.getText().toString());
+                } else {
+                    showAlert();
+                }
+            }
+        });
+    }
+
+    private void signInWithEmailAndPass(String email, String pass) {
+        auth.signInWithEmailAndPassword(email, pass)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            showHome(task.getResult().getUser().getEmail() != null ? task.getResult().getUser().getEmail() : "", ProviderType.BASIC, AuthActivity.this);
+                        } else {
+                            showAlert();
+                        }
+                    }
+                });
+    }
+
+    private void showAlert() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Error");
+        builder.setMessage("Se ha producido un error autenticando al usuario");
+        builder.setPositiveButton("Aceptar", null);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private void showHome(String email, ProviderType provider, Context context) {
+        Intent homeIntent = new Intent(context, HomeActivity.class);
+        homeIntent.putExtra("provider_name", provider.name());
+        context.startActivity(homeIntent);
     }
 }
--- proyectoMovil1/app/src/main/java/edu/pucmm/icc451/HomeActivity.java
+++ proyectoMovil1/app/src/main/java/edu/pucmm/icc451/HomeActivity.java
@@ -1,6 +1,7 @@
 package edu.pucmm.icc451;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,46 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+
+enum ProviderType {
+    BASIC
+}
+
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseApp.initializeApp(this);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Setup
+        Bundle bundle = getIntent().getExtras();
+        String email = bundle != null ? bundle.getString("email") : null;
+        String provider = bundle != null ? bundle.getString("provider") : null;
+
+        setup(email != null ? email : "", provider != null ? provider : "");
+    }
+
+    private void setup(String email, String provider) {
+        setTitle("Inicio");
+        TextView emailTextView = findViewById(R.id.emailTextView);
+        TextView proveedorTextView = findViewById(R.id.proveedorTextView);
+
+        emailTextView.setText(email);
+        proveedorTextView.setText(provider);
+
+        findViewById(R.id.cerrarSesionButton).setOnClickListener(v -> {
+            FirebaseAuth.getInstance().signOut();
+            onBackPressed();
         });
     }
 }
