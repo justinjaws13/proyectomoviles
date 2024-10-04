777fd3d457c23267b161e642140dfe364c399f86
Justin Quijada Hernandez <justin_quijada@hotmail.com>
2024-10-03 23:51:30 -0400

    agregando dependencias de realtime database

--- proyectoMovil1/app/build.gradle.kts
+++ proyectoMovil1/app/build.gradle.kts
@@ -44,6 +44,8 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.3.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-analytics")
+    implementation(platform("com.google.firebase:firebase-bom:32.8.0"))
+    implementation("com.google.firebase:firebase-database")
     implementation ("com.google.firebase:firebase-auth:latest-version")
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
