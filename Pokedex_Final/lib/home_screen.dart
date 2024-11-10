a94189eadbc0a9d6a4cb0773e39bd9142ac82e44
Justin Quijada Hernandez <justin_quijada@hotmail.com>
2024-11-10 00:59:33 -0400

    Se ven todos los pokemons, la ui mas limpia y cada pokemon tiene el tipo que es, si es de fuego, agua etc

--- Pokedex_Final/lib/home_screen.dart
+++ Pokedex_Final/lib/home_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -25,45 +24,115 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
+
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            pokedex.isNotEmpty
-                ? GridView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 1.4,
-              ),
-              itemCount: pokedex.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  child: Column(
-                    children: [
-                      Text(pokedex[index]['name']),
-                      CachedNetworkImage(imageUrl: pokedex[index]['img']),
-                    ],
+      body: Stack(
+        children: [
+          // Imagen de fondo (pokeball)
+          Positioned(
+            top: -50,
+            right: -50,
+            child: Image.asset('images/pokeball2.png', width: 200, fit: BoxFit.fitWidth),
+          ),
+          // Contenido desplazable
+          Positioned(
+            top: 80,
+              left: 20,
+              child:Text("Pokedex",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black ),)
+          ),
+          Positioned(
+            top: 150,
+            bottom: 0,
+            width: width,
+            child: SingleChildScrollView(  // Añadimos SingleChildScrollView para evitar desbordamiento
+              child: Column(
+                children: [
+                  // Si hay datos del pokédex, mostramos el GridView
+                  pokedex.isNotEmpty
+                      ? GridView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 1.4,
+                    ),
+                    itemCount: pokedex.length,
+                    itemBuilder: (context, index) {
+                      var type = pokedex[index]['type'][0];
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 12),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.green,
+                            borderRadius: const BorderRadius.all(Radius.circular(20)),
+                          ),
+                          child: Stack(
+                            children: [
+                              Positioned(
+                                top: 30,
+                                left: 10,
+                                child: Text(
+                                  pokedex[index]['name'],
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                top: 52,
+                                left: 10,
+                                child: Container(
+                                  decoration: const BoxDecoration(
+                                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                                    color: Colors.black26,
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(left: 8.0, right: 8.0, top: 4, bottom: 4),
+                                    child: Text(
+                                      type.toString(),
+                                      style: const TextStyle(color: Colors.white),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                bottom: 5,
+                                right: 5,
+                                child: CachedNetworkImage(
+                                  imageUrl: pokedex[index]['img'],
+                                  height: 100,
+                                  fit: BoxFit.fitHeight,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  )
+                      : const Center(
+                    child: CircularProgressIndicator(),
                   ),
-                );
-              },
-            )
-                : const Center(
-              child: CircularProgressIndicator(),
+                ],
+              ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 
-  //llamada a la api para conseguir la informacion de los pokemons
+  // Llamada a la API para obtener los datos de Pokémon
   void conseguirPokemonData() async {
     var url = Uri.https("raw.githubusercontent.com", "/Biuni/PokemonGO-Pokedex/master/pokedex.json");
     var client = http.Client();
     try {
-      //para esperar por el servidor porque hay imagenes que no cargaban y daban error
+      // Esperamos la respuesta del servidor (en caso de que las imágenes no se carguen rápidamente)
       var response = await client.get(url).timeout(const Duration(seconds: 10));
       if (response.statusCode == 200) {
         var decodedJsonData = jsonDecode(response.body);
@@ -74,7 +143,7 @@ class _HomeScreenState extends State<HomeScreen> {
         print('Error: ${response.statusCode}');
       }
     } catch (error) {
-      print('Network error: $error');
+      print('Error de red: $error');
     } finally {
       client.close();
     }
