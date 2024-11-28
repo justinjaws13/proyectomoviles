fed0097cb3e1fae2663ae40d240ef681c0300785
Justin Quijada Hernandez <justin_quijada@hotmail.com>
2024-11-28 19:09:53 -0400

    Revert "Le aplique paginacion y separe el archivo de GrapQL"
    This reverts commit f3885a702c4def3970499a661969f398f8999865.

--- Pokedex_Final/lib/home_screen.dart
+++ Pokedex_Final/lib/home_screen.dart
@@ -1,11 +1,55 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 import 'pokemon_details_screen.dart';
-import 'package:pokedex_final/queries/graphql_queries.dart';
 
+// Consulta GraphQL
+const String getPokemonListQuery = """
+  query {
+    pokemon_v2_pokemon(order_by: {id: asc}) {
+      id
+      name
+      height
+      weight
+      pokemon_v2_pokemonspecy {
+        generation_id
+        evolves_from_species_id
+        pokemon_v2_evolutionchain {
+          pokemon_v2_pokemonspecies(order_by: {id: asc}) {
+            name
+          }
+        }
+      }
+      pokemon_v2_pokemontypes {
+        pokemon_v2_type {
+          name
+        }
+      }
+      pokemon_v2_pokemonabilities {
+        pokemon_v2_ability {
+          name
+        }
+      }
+      pokemon_v2_pokemonstats {
+        base_stat
+        pokemon_v2_stat {
+          name
+        }
+      }
+      pokemon_v2_pokemonmoves(limit: 5) {  
+        pokemon_v2_move {
+          name
+        }
+      }
+      pokemon_v2_pokemonsprites {
+        sprites
+      }
+    }
+  }
+""";
 
+// Iconos para los tipos de Pokémon
 const Map<String, IconData> _typeIcons = {
   'grass': Icons.grass,
   'fire': Icons.local_fire_department,
@@ -35,17 +79,11 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
-  static const int pageSize = 20; // Tamaño de cada página
-  int offset = 0;
-  bool isLoadingMore = false;
-  bool hasMoreData = true;
-  List<Map<String, dynamic>> pokedex = [];
-
   String? selectedType;
   int? selectedGeneration;
   String searchQuery = "";
   late AnimationController _animationController;
-  final ScrollController _scrollController = ScrollController();
+  final ScrollController _scrollController = ScrollController(); // Inicialización directa
   final TextEditingController searchController = TextEditingController();
 
   final List<String> types = [
@@ -66,7 +104,7 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
 
   @override
   void dispose() {
-    _scrollController.dispose();
+    _scrollController.dispose(); // esto limpia el controlador para evitar fugas de memoria
     _animationController.dispose();
     super.dispose();
   }
@@ -78,6 +116,7 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
       appBar: AppBar(
         title: GestureDetector(
           onTap: () {
+            // Acción al tocar "Pokédex": hace scroll hacia arriba
             _scrollController.animateTo(
               0.0,
               duration: const Duration(milliseconds: 500),
@@ -94,6 +133,7 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
       body: Column(
         children: [
           const SizedBox(height: 10),
+          // Barra de búsqueda y filtros
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 10.0),
             child: Column(
@@ -171,164 +211,136 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
             child: Query(
               options: QueryOptions(
                 document: gql(getPokemonListQuery),
-                variables: {
-                  'limit': pageSize,
-                  'offset': offset,
-                },
               ),
               builder: (QueryResult result, {fetchMore, refetch}) {
-                if (result.isLoading && pokedex.isEmpty) {
+                if (result.isLoading) {
                   return const Center(child: CircularProgressIndicator());
                 }
                 if (result.hasException) {
                   return Center(child: Text('Error al cargar datos: ${result.exception.toString()}'));
                 }
 
-                final newPokedex = (result.data?['pokemon_v2_pokemon'] as List?)
-                    ?.cast<Map<String, dynamic>>() ?? [];
+                final pokedex = (result.data?['pokemon_v2_pokemon'] as List?)
+                    ?.cast<Map<String, dynamic>>() ??
+                    [];
 
-                if (!isLoadingMore) {
-                  pokedex.addAll(newPokedex);
-                  hasMoreData = newPokedex.length == pageSize;
-                }
-                isLoadingMore = false;
+                // Filtro de Pokémon
+                final filteredPokedexByTypeAndGen = pokedex.where((pokemon) {
+                  final types = (pokemon['pokemon_v2_pokemontypes'] as List?)
+                      ?.map((typeData) =>
+                      (typeData['pokemon_v2_type']['name'] as String).toLowerCase())
+                      .toList() ??
+                      [];
+                  final generation = pokemon['pokemon_v2_pokemonspecy']?['generation_id'];
 
-                final filteredPokedex = _applyFilters(pokedex);
+                  final typeMatches = selectedType == null ||
+                      types.contains(selectedType!.toLowerCase());
+                  final generationMatches = selectedGeneration == null ||
+                      generation == selectedGeneration;
 
-                return NotificationListener<ScrollNotification>(
-                  onNotification: (scrollInfo) {
-                    if (!isLoadingMore &&
-                        hasMoreData &&
-                        scrollInfo.metrics.pixels == scrollInfo.metrics.maxScrollExtent) {
-                      setState(() {
-                        isLoadingMore = true;
-                        offset += pageSize;
-                      });
-                      fetchMore!(FetchMoreOptions(
-                        variables: {'offset': offset},
-                        updateQuery: (previous, fetchMoreResult) {
-                          return {
-                            'pokemon_v2_pokemon': [
-                              ...previous?['pokemon_v2_pokemon'] ?? [],
-                              ...fetchMoreResult?['pokemon_v2_pokemon'] ?? [],
-                            ],
-                          };
-                        },
-                      ));
-                    }
-                    return false;
-                  },
-                  child: GridView.builder(
-                    controller: _scrollController,
-                    padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      childAspectRatio: 0.9,
-                      mainAxisSpacing: 10,
-                      crossAxisSpacing: 10,
-                    ),
-                    itemCount: filteredPokedex.length + (hasMoreData ? 1 : 0),
-                    itemBuilder: (context, index) {
-                      if (index == filteredPokedex.length) {
-                        return const Center(child: CircularProgressIndicator());
-                      }
-                      return _buildPokemonCard(filteredPokedex[index]);
-                    },
+                  return typeMatches && generationMatches;
+                }).toList();
+
+                // Aplicar búsqueda
+                final filteredPokedex = filteredPokedexByTypeAndGen.where((pokemon) {
+                  final index = pokedex.indexOf(pokemon) + 1;
+                  final name = (pokemon['name'] as String).toLowerCase();
+
+                  return searchQuery.isEmpty ||
+                      name.contains(searchQuery) ||
+                      index.toString() == searchQuery;
+                }).toList();
+
+                return GridView.builder(
+                  controller: _scrollController,
+                  padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 0.9, // Tamaño más proporcional
+                    mainAxisSpacing: 10,
+                    crossAxisSpacing: 10,
                   ),
+                  itemCount: filteredPokedex.length,
+                  itemBuilder: (context, index) {
+                    final pokemon = filteredPokedex[index];
+                    final types = (pokemon['pokemon_v2_pokemontypes'] as List?)
+                        ?.map((typeData) => typeData['pokemon_v2_type']['name'] as String)
+                        .toList() ??
+                        ['Unknown'];
+
+                    final spriteData = pokemon['pokemon_v2_pokemonsprites']?.first['sprites'];
+                    final imageUrl = spriteData != null ? spriteData['front_default'] : null;
+
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (_) => PokemonDetailScreen(
+                              fullPokedex: pokedex,
+                              filteredPokedex: filteredPokedex,
+                              currentIndex: index,
+                            ),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              _getTypeColor(types.first).withOpacity(0.8),
+                              _getTypeColor(types.first).withOpacity(0.4),
+                            ],
+                            begin: Alignment.topLeft,
+                            end: Alignment.bottomRight,
+                          ),
+                          borderRadius: const BorderRadius.all(Radius.circular(20)),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(top: 10, left: 10),
+                              child: Text(
+                                pokemon['name'].toUpperCase(),
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(left: 10, bottom: 5),
+                              child: Row(
+                                children: types.map((type) {
+                                  return Icon(
+                                    _typeIcons[type.toLowerCase()] ?? Icons.help,
+                                    color: Colors.white,
+                                    size: 18,
+                                  );
+                                }).toList(),
+                              ),
+                            ),
+                            Expanded(
+                              child: Center(
+                                child: imageUrl != null
+                                    ? CachedNetworkImage(imageUrl: imageUrl)
+                                    : const Icon(Icons.image_not_supported, color: Colors.white),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
                 );
               },
             ),
           ),
         ],
-      ),
-    );
-  }
-
-  List<Map<String, dynamic>> _applyFilters(List<Map<String, dynamic>> pokedex) {
-    return pokedex.where((pokemon) {
-      final types = (pokemon['pokemon_v2_pokemontypes'] as List?)
-          ?.map((typeData) => typeData['pokemon_v2_type']['name'].toLowerCase())
-          .toList() ??
-          [];
-      final generation = pokemon['pokemon_v2_pokemonspecy']?['generation_id'];
-
-      final typeMatches = selectedType == null || types.contains(selectedType!.toLowerCase());
-      final generationMatches = selectedGeneration == null || generation == selectedGeneration;
-      final nameMatches = searchQuery.isEmpty || pokemon['name'].toLowerCase().contains(searchQuery);
-
-      return typeMatches && generationMatches && nameMatches;
-    }).toList();
-  }
-
-  Widget _buildPokemonCard(Map<String, dynamic> pokemon) {
-    final types = (pokemon['pokemon_v2_pokemontypes'] as List?)
-        ?.map((typeData) => typeData['pokemon_v2_type']['name'])
-        .toList() ?? ['Unknown'];
-    final spriteData = pokemon['pokemon_v2_pokemonsprites']?.first['sprites'];
-    final imageUrl = spriteData != null ? spriteData['front_default'] : null;
-
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (_) => PokemonDetailScreen(
-              fullPokedex: pokedex,
-              filteredPokedex: pokedex,
-              currentIndex: pokedex.indexOf(pokemon),
-            ),
-          ),
-        );
-      },
-      child: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              _getTypeColor(types.first).withOpacity(0.8),
-              _getTypeColor(types.first).withOpacity(0.4),
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-          borderRadius: const BorderRadius.all(Radius.circular(20)),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 10, left: 10),
-              child: Text(
-                pokemon['name'].toUpperCase(),
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-                overflow: TextOverflow.ellipsis,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 10, bottom: 5),
-              child: Row(
-                children: types.map((type) {
-                  return Icon(
-                    _typeIcons[type.toLowerCase()] ?? Icons.help,
-                    color: Colors.white,
-                    size: 18,
-                  );
-                }).toList(),
-              ),
-            ),
-            Expanded(
-              child: Center(
-                child: imageUrl != null
-                    ? CachedNetworkImage(imageUrl: imageUrl)
-                    : const Icon(Icons.image_not_supported, color: Colors.white),
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }
